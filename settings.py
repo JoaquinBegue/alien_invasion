7aa17312c6d4dd63e4b3f6f464b3c2ccf1519bf1
Joaquin Begue <joaquinbegue1204@gmail.com>
2022-07-25 16:24:15 -0300

    Added bullet firing.
    Added bullet's module, settings and functions.

--- settings.py
+++ settings.py
@@ -4,9 +4,16 @@ class Settings():
     def __init__(self):
         """Initialize the game's settings."""
         # Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 800
+        self.screen_height = 400
         self.bg_color = (0, 0, 0)
 
         # Ship settings
         self.ship_speed_factor = 1.5
+
+        # Bullet settings
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 200, 200, 0
+        self.bullets_allowed = 3
